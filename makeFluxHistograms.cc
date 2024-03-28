2a24cf3d4e3e0fd4eccdcc7e922ef430bc3be9f9
Tony Wood <apwood@uh.edu>
2024-03-28 17:18:51 -0500

    Add file count

--- makeFluxHistograms.cc
+++ makeFluxHistograms.cc
@@ -17,21 +17,33 @@
 #include <unordered_map>
 #include <vector>
 
-std::vector<std::string> getFileList(const char* file_name)
+std::vector<std::string> getFileList(std::string_view file_name)
 {
   std::vector<std::string> listOfFiles;
 
-  std::ifstream fileList(file_name);
-
-  std::string line;
+  std::ifstream fileList(file_name.data());
 
   if (!fileList.is_open()) {
     std::cout << "unable to open file " << file_name << "\n";
     return listOfFiles;
   }
 
+  int numFiles = 0;
+  std::string tmp;
+  while(std::getline(fileList, tmp)) {
+    numFiles++;
+  }
+
+  std::cout << "Number of files in the list: " << numFiles << '\n';
+
+  fileList.clear();
+  fileList.seekg(0, std::ios::beg);
+
+  listOfFiles.reserve(numFiles);
+
+  std::string line;
   while (std::getline(fileList, line)) {
-    listOfFiles.push_back(line);
+    listOfFiles.emplace_back(line);
   }
 
   return listOfFiles;
