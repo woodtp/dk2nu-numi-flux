7dcecdcdce40a83aa006eba8252140f266d260b7
Tony Wood <apwood@uh.edu>
2024-04-05 14:54:33 -0500

    setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-setup dk2nu v01_05_01b -q e15:prof
-setup clang v14_0_6c
-setup cmake v3_21_4
+export DK2NU=$PWD/dk2nu
+export DK2NU_INC=$DK2NU/tree
+export DK2NU_LIB=$DK2NU/build/lib
+
+source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+setup root v6_28_10a -q e28:p3915:prof
+source ./venv/bin/activate
